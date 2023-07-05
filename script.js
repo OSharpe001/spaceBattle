0aff33ab9415dfe0fd4b2e7b57e66afc79c2604c
O. Sharpe <osharpe001@gmail.com>
2023-07-05 00:04:26 -0400

    added: startin statement. image assets. window alerts.

--- script.js
+++ script.js
@@ -2,60 +2,57 @@ const fireLaserBtn = document.querySelector(".fire-laser");
 const retreatBtn = document.querySelector(".retreat");
 const resetBtn = document.querySelector(".reset-game");
 const startingStory = document.querySelector(".story");
-// let captainName;
-// let captainName = startGame();
-// let captainName = setTimeout(()=>startGame(), 8000);
+const firstGameStartText = document.querySelector(".first-game");
+let initialEnemyCount = 6;
 
 class EarthShip {
-    constructor(shipName, /*captain = captainName,*/ hull = 20, firepower = 5, accuracy = 0.7) {
+    constructor(shipName, hull = 20, firepower = 5, accuracy = 0.7) {
       this.name = shipName;
-    //   this.captain = captain;
       this.hull = hull;
       this.firepower = firepower;
       this.accuracy = accuracy;
     };
 
     fireLaser(enemy) {
-        // console.log(`Captain ${this.captain}.There's ${enemyFighters.length} enemies left!`);
-        console.log(`Captain, there's ${enemyFighters.length} enemies left!`);
-        // console.log(`The current combatant is ${currentCombatant.name}.`);
         console.log(`${enemy.name}'s laser strength is ${enemy.firepower}!`);
         console.log(`${enemy.name}'s remaining hull strength is ${enemy.hull}.`);
         console.log(`Engaging ${enemy.name}!`);
-        fireLaserBtn.classList.toggle("hidden");
-        if (enemyFighters.length < 6) {
-            retreatBtn.classList.toggle("hidden");
-        };
-        // retreatBtn.classList.toggle("hidden");
+        window.alert(`${enemy.name}'s laser strength is ${enemy.firepower}!\n${enemy.name}'s remaining hull strength is ${enemy.hull}.\nEngaging ${enemy.name}!`);
         let chance = Math.random();
         if (chance < this.accuracy) {
             console.log(`The ${this.name} has hit ${enemy.name}'s craft causing ${this.firepower} damage!`);
+            window.alert(`The ${this.name} has hit ${enemy.name}'s craft causing ${this.firepower} damage!`);
             enemy.hull -= this.firepower;
             if (enemy.hull > 0) {
-                // console.log(`Captain ${this.captain}, ${enemy.name}'s hull strength is now ${enemy.hull}.`);
                 console.log(`Captain, ${enemy.name}'s hull strength is now ${enemy.hull}.`);
+                window.alert(`Captain, ${enemy.name}'s hull strength is now ${enemy.hull}.`);
                 enemy.fireLaser();
             } else {
-                // console.log(`Congrats Captain ${this.captain}! You've defeated ${enemy.name}`);
-                console.log(`Congrats Captain! You've defeated ${enemy.name}`);
+                console.log(`Congrats Captain! You've defeated ${enemy.name}.`);
                 enemyFighters.shift();
                 currentCombatant = enemyFighters[0];
+                if (enemyFighters.length > 0) {
+                    console.log(`Captain, there's ${enemyFighters.length} enemies left!`);
+                    window.alert(`Congrats Captain! You've defeated ${enemy.name}.\nThere's ${enemyFighters.length} enemies left!`);
+                };
                 if (currentCombatant) {
-                    // console.log(`Captain ${this.captain}, our remaining hull strength is ${this.hull}`);
                     console.log(`Captain, our remaining hull strength is ${this.hull}`);
-                console.log("What's the next move, Sir?");
-                console.log("--------------------------------------------");
-                setTimeout(() => {fireLaserBtn.className = "fire-laser button"; retreatBtn.className = "retreat button"}, 2000);
+                    console.log("What's the next move, Sir?");
+                    console.log("--------------------------------------------");
+                    window.alert(`Captain, our remaining hull strength is ${this.hull}\nWhat's the next move, Sir?`);
+                    retreatBtn.className = "retreat button";
                 } else {
-                    // console.log(`Captain ${this.captain} of the ${this.name} returns triumphant!`);
                     console.log(`The ${this.name} returns triumphant!`);
-                    console.log('Earth is safe for another day. . . ');
-                    setTimeout(() => resetBtn.classList.toggle("hidden"), 2000);
+                    console.log('Earth is safe for another day. . .');
+                    setTimeout(() => window.alert(`The ${this.name} returns triumphant!\nEarth is safe for another day. . .`), 50);
+                    fireLaserBtn.classList.toggle("hidden");
+                    retreatBtn.classList.toggle("hidden");
+                    setTimeout(() => {resetBtn.classList.toggle("hidden")}, 2000);
                 };
             };
         } else {
-            // console.log(`Captain ${this.captin}, we missed the alien ship!\nWe need better weapons...`);
             console.log(`Captain, we missed the alien ship!\nWe need better weapons...`);
+            window.alert(`Captain, we missed the alien ship!\nWe need better weapons...`);
             enemy.fireLaser();
         };
     };
@@ -63,8 +60,8 @@ class EarthShip {
     retreat() {
         fireLaserBtn.classList.toggle("hidden");
         retreatBtn.classList.toggle("hidden");
-        // console.log(`The aliens have broken through Earth's defenses!`);
         console.log(`Game Over!`);
+        setTimeout(() => window.alert("Game Over"), 50);
         setTimeout(() => resetBtn.className = "reset-game button", 2000);
     };
 };
@@ -80,22 +77,24 @@ class AlienShip {
 
       fireLaser() {
         if (Math.random() < this.accuracy) {
-            console.log(`You've been hit!`);
+            console.log(`We've been hit!`);
             this.enemy.hull -= this.firepower;
             if (this.enemy.hull > 0) {
-                // console.log(`Captain ${this.enemy.captain}, our remaining hull strength is ${this.enemy.hull}`);
                 console.log(`Captain, our remaining hull strength is ${this.enemy.hull}`);
+                window.alert(`We've been hit!\nOur remaining hull strength is ${this.enemy.hull}`);
                 this.enemy.fireLaser(currentCombatant);
             } else {
-                // console.log(`Captain ${this.enemy.captain} of the ${this.enemy.name} has failed! Earth is doomed!`);
+                fireLaserBtn.classList.toggle("hidden");
+                retreatBtn.classList.toggle("hidden");
                 console.log(`The ${this.enemy.name} has failed! Earth is doomed!`);
                 console.log("Game Over");
+                setTimeout(()=>window.alert(`The ${this.enemy.name} has failed! Earth is doomed!\nGame Over`), 50);
                 setTimeout(() => resetBtn.className = "reset-game button", 2000);
             };
         } else {
             console.log(`Whew! ${this.name} missed us!`);
-            // console.log(`Captain ${this.enemy.captain}, our remaining hull strength is stll at ${this.enemy.hull}`);
-            console.log(`Captain, our remaining hull strength is stll at ${this.enemy.hull}`);
+            console.log(`Captain, our remaining hull strength is still at ${this.enemy.hull}`);
+            window.alert(`Whew! ${this.name} missed us!\nOur remaining hull strength is still at ${this.enemy.hull}`);
             this.enemy.fireLaser(currentCombatant);
         };
     };
@@ -105,7 +104,7 @@ const USS_Assembly = new EarthShip("USS Assembly");
 
 const enemyFighters = [];
 
-for (i = 1; i <= 6; i++) {
+for (i = 1; i <= initialEnemyCount; i++) {
     let alienShip = new AlienShip(`Alien Ship #${i}`);
     enemyFighters.push(alienShip);
 };
@@ -118,49 +117,35 @@ resetBtn.addEventListener("click", () => window.location = "index.html");
 
 let sillyResponse=0;
 const startGame = () => {
-    // sillyResponse=0;
     startGamePrompt = window.prompt("Are you ready to begin?");
-    if (sillyResponse >= 2) {
+    if (sillyResponse >= 3 && (startGamePrompt != "y" || startGamePrompt != "yes")) {
         window.alert(`Do you think this is a game, Captain?!? (🤣🤣🤣)\nThe WHOLE WORLD is depending on you, right now!\nThere's no going back or "playing the fool"!\nNow let's go tan some alien hide!`);
         sillyResponse = 0;
-        setTimeout(()=>startGame(), 8000);
+        setTimeout(()=>startGame(), 6000);
     } else if (!startGamePrompt) {
         sillyResponse++;
         window.alert(`Don't ignore your responsiblities, Captain!\nType "y" or "yes" to start game.`);
-        setTimeout(()=>startGame(), 8000);
+        setTimeout(()=>startGame(), 6000);
     } else {
         startGamePrompt = startGamePrompt.toLowerCase();
         if (startGamePrompt === "y" || startGamePrompt === "yes") {
-            // captainName = getName();
-            setTimeout(startingStory.classList.add("hidden"), 1000);
-            setTimeout(() => fireLaserBtn.classList.toggle("hidden"), 2000);
-            // console.log("getName(): ", captainName);
-            // var captainName = captainName;
-            // return captainName;
+            setTimeout(()=>startingStory.classList.add("hidden"), 1000);
+            setTimeout(()=>firstGameStartText.classList.toggle("hidden"), 1500);
+            setTimeout(()=>firstGameStartText.classList.toggle("hidden"), 5500);
+            setTimeout(() => fireLaserBtn.classList.toggle("hidden"), 6000);
         } else if (startGamePrompt === "n" || startGamePrompt === "no") {
             sillyResponse++;
             window.alert("We know that this is an arduous mission but the world needs you, Captain and time is of the essence!\nWe'll give you a few more seconds to temper your nerves...");
-            setTimeout(()=>startGame(), 8000);
+            setTimeout(()=>startGame(), 6000);
         } else {
             sillyResponse++;
             window.alert('Type "y" or "yes" to start game.');
-            setTimeout(()=>startGame(), 8000);
+            setTimeout(()=>startGame(), 6000);
         };
     };
 };
-setTimeout(()=>startGame(), 8000);
+setTimeout(()=>startGame(), 6000);
 
-// const getName = () => {
-//     captainNamePrompt = window.prompt("What's your name, Captain?");
-//     if (!captainNamePrompt) {
-//         alert("We'll need to know what name to place on your medals,... (or tombstone)...");
-//         getName();
-//     } else {
-//         console.log("captainNamePrompt: ", captainNamePrompt)
-//         captainName = captainNamePrompt;
-//         return captainNamePrompt;
-//     };
-// };
 
 // console.log(`There's ${enemyFighters.length} enemies left!`);
 // console.log(`The current combatant is ${currentCombatant.name}.`);
